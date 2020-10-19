b59e705ce38bc523c975b32da20c499677a26c3d
sp055 <sp055938@student.ath.edu.pl>
2020-10-19 18:58:20 +0200

    final

---
+++ ZD3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ZD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica();
+            Console.WriteLine("=======================");
+            Console.WriteLine();
+            Console.Write("Wpisz tekst: ");
+            Zdanie(Console.ReadLine());                                         // czyta tekst wpisany do konsoli i przesyla do funkcji
+        }
+
+        static void Zdanie(string tekst)
+        {
+            tekst = tekst.Trim();                                               // usuwa spacje z początku i końca tekstu (jeśli na końcu nie ma innego znaku)
+
+            if (string.IsNullOrWhiteSpace(tekst))                               // sprawdza czy tekst jest pusty lub zawiera tylko spacje jeśli tak wykonuje instrukcje
+            {
+                System.Console.WriteLine("Nie wpisałeś tekstu.");
+                System.Environment.Exit(1);                                     // wychodzi z programu
+            }
+            else if (!char.IsLetter(tekst, 0))                                  // sprawdza czy pierwszy znak w tekscie jest literą jeśli nie jest to wykonuje instrukcje
+            {
+                System.Console.WriteLine("Nie zacząłeś od litery.");
+                System.Environment.Exit(1);                                     // wychodzi z programu
+            }
+
+            tekst = char.ToUpper(tekst[0]) + tekst.Substring(1);                // podnosi pierwszy znak do UPPERCASE i dodaje resztę tekstu ze zmiennej tekst od pozycji 1
+
+            if (tekst.EndsWith("."))                                            // sprawdza czy kończy się z kropką
+            {
+                Console.WriteLine(tekst);
+            }
+            else
+            {
+                Console.WriteLine(tekst + ".");                                 // dodaje kropkę do tekstu
+            }
+        }
+
+        static void Tablica()
+        {
+            Random rnd = new Random();                                          // zastosowanie klasy random do losowania liczb
+            int pierwszywymiar = 10;
+
+            int[][] tablica = new int[pierwszywymiar][];                        // stworzenie tablicy
+
+            for (int i = 0; i < tablica.Length; i++)                            // wykonuje pętlę tyle razy ile wynosi wielkość pierwszego wymiaru
+            {
+                tablica[i] = new int[rnd.Next(10)];                             // losowanie wielkości drugiego wymiaru
+
+                for (int j = 0; j < tablica[i].Length; j++)                     // wykonuje pętlę tyle razy ile wynosi wartość drugiego wymiaru dla wybranego wymiaru pierwszego
+                {
+                    tablica[i][j] = 1;                                          // do każdego pola przypisuje wartość 1
+                }
+            }
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    Console.Write("|" + tablica[i][j] + "|");                   // wypisuje wartości pól tablicy
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("=======================");
+            int suma = 0;
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    suma += tablica[i][j];                                      // dodaje każde pole do siebie 
+                }
+            }
+            Console.WriteLine($"Suma wszystkich pól: {suma}");                  // wynik
+        }
+    }
+}
